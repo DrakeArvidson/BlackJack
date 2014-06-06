3fb97aef8903742d4fcdb724fa91d6dbeb44b040
Drake Arvidson <darvidson@denniskirk.com>
2014-06-06 16:41:20 -0500

    Added a few new classes

--- src/main/BlackJackGame.java
+++ src/main/BlackJackGame.java
@@ -1,8 +1,11 @@
 package main;
 
+import gui.BlackJackFrame;
+
 public class BlackJackGame {
 
 	public static void main(String[] args) {
 		//This is where the game will be run
+		new BlackJackFrame();
 	}
 }
